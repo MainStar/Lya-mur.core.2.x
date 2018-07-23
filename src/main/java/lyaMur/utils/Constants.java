272a951daa540ff8645069b34e540fa36c9ae48c
MainStar <vladislav.postoyan@yandex.ru>
2018-07-23 13:29:32 +0300

    Update Constants.java

--- src/main/java/lyaMur/utils/Constants.java
+++ src/main/java/lyaMur/utils/Constants.java
@@ -3,7 +3,7 @@ package lyaMur.utils;
 public class Constants {
 
     //Html dir
-    public static final String HTML_DIR = "/resources/pages";
-    public static final String HTML_ADMIN_DIR = "resources/pages/admin";
+    public static final String HTML_DIR = "src/main/webapp/resources/pages";
+    public static final String HTML_ADMIN_DIR = "src/main/webapp/resources/pages/admin";
 
 }
